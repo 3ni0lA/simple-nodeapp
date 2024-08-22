5bcde77cdca4f7d05844012495f320de22b65b67
Adewale Eniola <104868415+3ni0lA@users.noreply.github.com>
2024-08-22 22:30:18 +0100

    Added a health check enpoint to the app

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get('/api/greeting', (req, res) => {
   res.json({ message: 'Hello from Node.js API!' });
 });
 
+
+// New health check endpoint
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' }); // Returns a JSON object with status ok
+});
+
+
 app.listen(port, () => {
   console.log(`Node.js app listening at http://localhost:${port}`);
 });

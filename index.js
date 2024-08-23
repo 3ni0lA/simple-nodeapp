e6718a440d164df6e2dbbcb3b408f40d2fe5e0d2
Adewale Eniola <104868415+3ni0lA@users.noreply.github.com>
2024-08-23 18:16:31 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the Node.js app!');
+});
+
 app.get('/api/greeting', (req, res) => {
   res.json({ message: 'Hello from Node.js API!' });
 });
